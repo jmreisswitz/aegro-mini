57ed0f5a13b6252a2a44e5efa4c9c6497416dba7
Mario <jmreisswitz@gmail.com>
2020-08-13 20:47:03 -0300

    add spring initializr

---
+++ backend/src/main/java/com/jmreisswitz/aegromini/AegroMiniApplication.java
@@ -0,0 +1,13 @@
+package com.jmreisswitz.aegromini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegroMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AegroMiniApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jmreisswitz/aegromini/AegroMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmreisswitz.aegromini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegroMiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
