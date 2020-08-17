68a71fc0b7e51ef38e69b08671d57421f29eef9d
Mario <jmreisswitz@gmail.com>
2020-08-16 23:31:26 -0300

    add tests to add planet case

---
+++ backend/src/test/java/com/jmreisswitz/aegromini/usecases/farm/AddFarmUseCaseTest.java
@@ -0,0 +1,70 @@
+package com.jmreisswitz.aegromini.usecases.farm;
+
+import com.jmreisswitz.aegromini.domain.Farm;
+import com.jmreisswitz.aegromini.ports.repository.FarmRepository;
+import org.apache.commons.lang.RandomStringUtils;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.stubbing.Answer;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+class AddFarmUseCaseTest {
+    @Mock
+    private FarmRepository farmRepository;
+
+    private AddFarmUseCase addFarmUseCase;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        addFarmUseCase = spy(new AddFarmUseCase(farmRepository));
+    }
+
+    @AfterEach
+    void tearDown() {
+        addFarmUseCase = null;
+    }
+
+    @Test
+    @DisplayName("Given a valid farm" +
+            "When call save" +
+            "Should not throw IllegalArgumentException" +
+            "Should return a farm with id")
+    void save_allGood_shouldPass() {
+        // Arrange
+        String farmName = RandomStringUtils.randomAlphanumeric(10);
+        Farm farm = new Farm(null, farmName, null);
+        String fakeGeneratedId = RandomStringUtils.randomAlphabetic(10);
+        Answer<Farm> repositoryAnswer = invocation -> new Farm(fakeGeneratedId, farmName, null);
+        when(farmRepository.save(farm)).thenAnswer(repositoryAnswer);
+
+        // Act
+        Farm savedFarm = null;
+        try{
+            savedFarm = addFarmUseCase.execute(farm);
+        } catch (IllegalArgumentException illegalArgumentException){
+            fail("IllegalArgumentException");
+        }
+        Farm finalSavedFarm = savedFarm;
+
+        // Assert
+        assertAll(
+                () -> assertEquals(finalSavedFarm.getId(), fakeGeneratedId),
+                () -> assertEquals(finalSavedFarm.getName(), farmName)
+        );
+    }
+
+    @Test
+    @DisplayName("Given a null farm" +
+            "Should throw IllegalArgumentException")
+    void save_nullFarm_shouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> {addFarmUseCase.execute(null);}
+        );
+    }
+
+}
