666f58e088ed2fb51d7227b151a49decfbad1a97
José Mário Reisswitz <jmreisswitz@gmail.com>
2020-08-17 19:24:39 -0300

    add fields use case tests

---
+++ backend/src/test/java/com/jmreisswitz/aegromini/usecases/field/AddFieldUseCaseTest.java
@@ -0,0 +1,69 @@
+package com.jmreisswitz.aegromini.usecases.field;
+
+import com.jmreisswitz.aegromini.domain.Field;
+import com.jmreisswitz.aegromini.ports.repository.FieldRepository;
+import org.apache.commons.lang.RandomStringUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.stubbing.Answer;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+class AddFieldUseCaseTest {
+    @Mock
+    private FieldRepository fieldRepository;
+
+    private AddFieldUseCase addFieldUseCase;
+
+    private String fakeFarmId;
+    private String fakeFieldId;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        addFieldUseCase = spy(new AddFieldUseCase(fieldRepository));
+        fakeFarmId = RandomStringUtils.randomAlphanumeric(10);
+        fakeFieldId = RandomStringUtils.randomAlphanumeric(10);
+    }
+
+    @AfterEach
+    void tearDown() {
+        addFieldUseCase = null;
+        fakeFarmId = null;
+        fakeFieldId = null;
+    }
+
+    @Test
+    void execute_allGood_shouldPass() {
+        // Arrange
+        String fieldName = RandomStringUtils.randomAlphanumeric(10);
+        Field field = new Field(null, fakeFarmId, fieldName, 1);
+        Answer<Field> repositoryAnswer = invocation -> new Field(fakeFieldId, fakeFarmId, fieldName, 1);
+        when(fieldRepository.save(field)).thenAnswer(repositoryAnswer);
+
+        // Act
+        Field savedField = null;
+        try {
+            savedField = addFieldUseCase.execute(field);
+        } catch (IllegalArgumentException ex){
+            fail("IllegalArgumentException thrown");
+        }
+
+        // Assert
+        assertEquals(savedField.getId(), fakeFieldId);
+        assertEquals(savedField.getFarmId(), fakeFarmId);
+    }
+
+    @Test
+    void execute_nullField_shouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            addFieldUseCase.execute(null);
+        });
+    }
+
+}
